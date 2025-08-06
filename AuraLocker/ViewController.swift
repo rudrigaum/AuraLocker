2532e83a6f11f8371c1fdcacb83ad7d67787ce1b
rudrigocerqueira <rodrigocerqueirareis@gmail.com>
2025-08-06 16:19:17 -0300

    Initial Commit

---
+++ AuraLocker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AuraLocker
+//
+//  Created by Rodrigo Cerqueira Reis on 06/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
